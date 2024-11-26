59cf1d965f29088eb6cdc46520a72b7f67304c51
Antoine Gach <76480167+legrotitou@users.noreply.github.com>
2024-11-26 14:04:29 +0100

    mcv training (#7)
    * model créé, controlleur a faire
    
    * il reste la policy pundit a faire
    
    * added a private method in controller and a before_action
    
    ---------
    
    Co-authored-by: Jeskaa-dev <jeskaa@hotmail.fr>

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,10 +1,16 @@
 class ResourcesController < ApplicationController
+  before_action :set_resource,  only: [ :show ]
 
   def index
     @resources = Resource.all
   end
 
   def show
+  end
+
+  private
+
+  def set_resource
     @resource = Resource.find(params[:id])
   end
 end
