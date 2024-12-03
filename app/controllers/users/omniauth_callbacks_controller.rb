b4426011469bee8050b786cc6a9f0e6b560feb89
Jeskaa-dev <jeskaa@hotmail.fr>
2024-12-03 16:31:25 +0100

    hoempage (#67)
    * for heroku
    
    * debugg
    
    * home page changes
    
    * hoem pages changes

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,6 +12,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def failure
-    redirect_to root_path
+    redirect_to user_path(current_user)
   end
 end
