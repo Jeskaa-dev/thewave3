6da7e2d5f022a33d6fa11329482b11b0a8f6ac62
Vincent-lelong <vincent.lelong1995@gmail.com>
2024-12-03 15:03:01 +0100

    Sorted user skill (#60)
    * add portfolio
    
    * bundle install
    
    * initial commit
    
    * initial commit
    
    * last commit before pull
    
    * mise a jour de la user-skill index
    
    ---------
    
    Co-authored-by: Jeskaa-dev <jeskaa@hotmail.fr>

--- app/controllers/user_skills_controller.rb
+++ app/controllers/user_skills_controller.rb
@@ -4,6 +4,8 @@ class UserSkillsController < ApplicationController
     @skills = Skill.where(id: @user_skills.map(&:skill_id))
     authorize @user_skills
     @user = current_user
-    @commit_status = current_user.commit_status
+    @commit_status = @user.commit_status
+    @top_user_skills = UserSkill.order(rating: :desc).limit(3)
+    @bottom_user_skills = UserSkill.order(rating: :asc).limit(3)
   end
 end
