b7ff7ac303860ee5ce78ab76ed63b4e3ccb1ed41
Julie V. <julieveran31@gmail.com>
2024-12-03 10:15:58 +0100

    remove plotly (#57)

--- Gemfile
+++ Gemfile
@@ -76,4 +76,4 @@ end
 
 gem "tailwindcss-rails", "~> 3.0"
 gem 'simple_form-tailwind'
-gem 'plotlyjs-rails'
+# gem 'plotlyjs-rails'
--- app/javascript/plotly.js
+++ app/javascript/plotly.js
@@ -1,20 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const data = [{
-    type: 'scatterpolar',
-    r: [80, 60, 25, 40], /* ce sont les valeurs de chaque skill */
-    theta: ['HTML/CSS', 'Javascript', 'SQL & DB', 'Ruby on Rails'], /* ce sont les noms des skills */
-    fill: 'toself'
-  }]
+// document.addEventListener("DOMContentLoaded", () => {
+//   const data = [{
+//     type: 'scatterpolar',
+//     r: [80, 60, 25, 40], /* ce sont les valeurs de chaque skill */
+//     theta: ['HTML/CSS', 'Javascript', 'SQL & DB', 'Ruby on Rails'], /* ce sont les noms des skills */
+//     fill: 'toself'
+//   }]
 
-  const layout = {
-    polar: {
-      radialaxis: {
-        visible: true,
-        range: [0, 100] /* c'est l'échelle de 0 à 100 */
-      }
-    },
-      showlegend: false
-  }
-  Plotly.newPlot("tester", data, layout)
-}
-)
+//   const layout = {
+//     polar: {
+//       radialaxis: {
+//         visible: true,
+//         range: [0, 100] /* c'est l'échelle de 0 à 100 */
+//       }
+//     },
+//       showlegend: false
+//   }
+//   Plotly.newPlot("tester", data, layout)
+// }
+// )
